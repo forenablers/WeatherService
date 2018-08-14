84481a2f3fa2f99b69f4dc09cc8434886459bd9b
zavolokas <sergey.zavoloka@gmail.com>
2018-08-14 14:17:45 +0200

    set host 0.0.0.0

--- info.py
+++ info.py
@@ -13,11 +13,10 @@ app = Flask(__name__)
 
 @app.route('/weather')
 def getWeather():
-    print appId
     city = request.args.get('city')
     response = requests.get('http://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}&units=metric'.format(city, appId)).content
     weather = json.loads(response)
     return 'The weather in {0} is {1}'.format(city, weather["main"]["temp"])
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True,host='0.0.0.0')
